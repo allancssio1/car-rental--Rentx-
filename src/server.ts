940f984a81377edfa7bbf246cdfd6e918b9918cc
Allan Cassio <allan.cassio@oqvocequer.com.br>
2022-04-11 07:58:57 -0300

    create route from categories

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import express from "express";
+import { categoriesRoutes } from "./routes/Categories.routes";
 
 const app = express()
 
 app.use(express.json())
-
+// O "/categories" será posto automaticamente em todas as rotas de dentro do categoriesRoutes
+app.use("/categories", categoriesRoutes) 
 app.listen(3333)
