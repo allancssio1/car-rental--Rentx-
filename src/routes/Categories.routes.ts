77b46497741228214b715370b3c9d2c49ccd0e82
Allan Cassio <allan.cassio@oqvocequer.com.br>
2022-04-14 07:32:50 -0300

    interação com models usando typescript

--- src/routes/Categories.routes.ts
+++ src/routes/Categories.routes.ts
@@ -1,19 +1,21 @@
 import { Router } from "express"
-import { v4 as uuid} from 'uuid'
+import { Category } from "../models/Category"
 
 const categoriesRoutes = Router()
 
-const categories = []
+const categories: Category[] = []
 
 // Criação de categories. 
 categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body
 
-  const category = {
-    id: uuid(),
+  const category = new Category()
+
+  Object.assign(category, {
     name,
-    description
-  }
+    description,
+    created_at: new Date()
+  })
 
   categories.push(category)
 
