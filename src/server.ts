693a1431dfbed919e4fccc22418ddfbdbd47a741
Allan Cassio <allan.cassio@oqvocequer.com.br>
2022-06-30 11:06:23 -0300

    restructuring code

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,10 @@
 import express from "express";
-import { categoriesRoutes } from "./routes/Categories.routes";
-import { specificationsRoutes } from "./routes/Specifications.routes";
+import { router } from "./routes";
+import multer from "multer";
 
 const app = express()
 
 app.use(express.json())
-// O "/categories" será posto automaticamente em todas as rotas de dentro do categoriesRoutes
-app.use("/categories", categoriesRoutes) 
-app.use("/specifications", specificationsRoutes) 
+app.use(router)
+
 app.listen(3333)
