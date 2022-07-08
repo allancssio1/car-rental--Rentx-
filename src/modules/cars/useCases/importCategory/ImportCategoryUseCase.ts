bffcb69cb44a63f87558ad231e6d36a5eca6ef16
Allan Cassio <allan.cassio@oqvocequer.com.br>
2022-07-08 07:57:49 -0300

    implement csv-parse for read file

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,7 +1,20 @@
+import {parse} from 'csv-parse'
+import fs from 'fs'
 
 class ImportCategoryUseCase {
   execute(file: Express.Multer.File): void {
-    console.log("2", file)
+    const stream = fs.createReadStream(file.path)
+    /* csv-parse é uma lib para auxiliar na manipulação do arquivo
+      ele pode receber um tipo de limitador que irá separar o arquivo a partir deste.
+      por padrão ele ja entende que o limitador é a ',' (vírgula)
+      parse({";"}) */
+    const parseFile = parse()
+
+    stream.pipe(parseFile)
+
+    parseFile.on("data", async (line) => {
+      console.log("filllll", line)
+    })
   }
 }
 
