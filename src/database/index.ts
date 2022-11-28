608c5792e92853706d8a46d6d126738d426b1f86
Allan Cassio <allan.cassio@oqvocequer.com.br>
2022-11-27 21:01:52 -0300

    criated db with docker-compose

--- src/database/index.ts
+++ src/database/index.ts
@@ -6,7 +6,7 @@ interface IOptions {
 
 getConnectionOptions().then((options) => {
   const newOptions = options as IOptions;
-  newOptions.host = "car_rental";
+  newOptions.host = "database_ignite";
   createConnection({
     ...options,
   });
