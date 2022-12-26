9d2decc2fe97f1638e1c647721f9a6d744af5cd4
allancassio <allan.cassio1@gmail.com>
2022-12-26 14:07:06 -0300

    return port 5432

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -14,11 +14,9 @@ class CreateCategoryUseCase {
   ) {}
 
   async execute({ name, description }: IRequest): Promise<void> {
-    console.log("o name aqui =>", name);
     const categoryAlreadyExists = await this.categoriesRepository.findByName(
       name,
     );
-    console.log("categoryAlreadyExists =>", categoryAlreadyExists);
 
     if (categoryAlreadyExists) {
       throw new Error("Category already exists!");
