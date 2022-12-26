b8994f9c059baed0dd19998bc691d190061990f0
allancassio <allan.cassio1@gmail.com>
2022-12-26 10:07:50 -0300

    set port on postgres

--- src/modules/cars/useCases/createCategory/CreateCategoryController.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.ts
@@ -6,7 +6,7 @@ class CreateCategoryController {
   async handle(request: Request, response: Response): Promise<Response> {
     const { name, description } = request.body;
     const createCategoryUserCase = container.resolve(CreateCategoryUseCase);
-
+    console.log("o name aqui 1111 =>", name);
     await createCategoryUserCase.execute({ name, description });
 
     return response.status(201).send("inserido");
--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -14,14 +14,15 @@ class CreateCategoryUseCase {
   ) {}
 
   async execute({ name, description }: IRequest): Promise<void> {
-    // const categoryAlreadyExists = await this.categoriesRepository.findByName(
-    //   name,
-    // );
-    // console.log("categoryAlreadyExists =>", categoryAlreadyExists);
+    console.log("o name aqui =>", name);
+    const categoryAlreadyExists = await this.categoriesRepository.findByName(
+      name,
+    );
+    console.log("categoryAlreadyExists =>", categoryAlreadyExists);
 
-    // if (categoryAlreadyExists) {
-    //   throw new Error("Category already exists!");
-    // }
+    if (categoryAlreadyExists) {
+      throw new Error("Category already exists!");
+    }
 
     this.categoriesRepository.create({ name, description });
   }
