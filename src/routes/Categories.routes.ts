a79d3aed14ea22cd267fcbe5c95b2bc2e74bc550
allancassio <allan.cassio1@gmail.com>
2022-12-26 18:06:06 -0300

    includes inject on importCategoryUseCase

--- src/routes/Categories.routes.ts
+++ src/routes/Categories.routes.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import multer from "multer";
 
 import { CreateCategoryController } from "../modules/cars/useCases/createCategory/CreateCategoryController";
-import importCategoryController from "../modules/cars/useCases/importCategory";
+import { ImportCategoryController } from "../modules/cars/useCases/importCategory/ImportCategoryController";
 import listCategoriesController from "../modules/cars/useCases/listCategories";
 
 const categoriesRoutes = Router();
@@ -10,16 +10,20 @@ const categoriesRoutes = Router();
 const upload = multer({
   dest: "./tmp",
 });
+
 // Criação de categories.
 const createCategoryController = new CreateCategoryController();
+const importCategoryController = new ImportCategoryController();
 
 categoriesRoutes.post("/", createCategoryController.handle);
 // listagem das categorias;
 categoriesRoutes.get("/", (request, response) =>
   listCategoriesController().handle(request, response),
 );
-categoriesRoutes.post("/import", upload.single("file"), (request, response) =>
-  importCategoryController().handle(request, response),
+categoriesRoutes.post(
+  "/import",
+  upload.single("file"),
+  importCategoryController.handle,
 );
 
 export { categoriesRoutes };
