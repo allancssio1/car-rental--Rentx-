974e29e9e2973ae8b32aea2db5a197c1ebe327e8
allancassio <allan.cassio1@gmail.com>
2022-12-29 15:04:57 -0300

    create entities users

---
+++ src/modules/accounts/entities/User.ts
@@ -0,0 +1,33 @@
+import { v4 as UUID } from "uuid";
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
+
+@Entity("users")
+export class User {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  driver_license: string;
+
+  @Column()
+  isAdmin: boolean;
+
+  @CreateDateColumn()
+  craeted_at: string;
+
+  constructor() {
+    if (!this.id) this.id = UUID();
+  }
+}
